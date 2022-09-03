2e3dcc52f6568427cdd69fd8c8e5b11104fdcbdf
Emmanuel <emma145@hotmail.es>
2022-09-03 09:45:15 -0500

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const path = require('path');
+const BUILD_DIR = path.resolve(__dirname, './build');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/react']
+            }
+        },
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'client/index.html'
+    })
+  ]
+};
