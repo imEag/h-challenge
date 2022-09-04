90dca9a58f6808689d8e0696f3ad602bd26138ac
Emmanuel <emma145@hotmail.es>
2022-09-03 23:00:48 -0500

    create conditions to find buildins according to query

--- server/building/buildingService.js
+++ server/building/buildingService.js
@@ -1,7 +1,25 @@
 const Building = require('./buildingSchema');
 
 const getBuildings = async query => {
-    const result = await Building.find({});
+    const conditions = [
+        {
+            "name": { $regex: query, $options: 'i' }
+        },
+        {
+            "address": { $regex: query, $options: 'i' }
+        },
+        {
+            "city": { $regex: query, $options: 'i' }
+        },
+        {
+            "state": { $regex: query, $options: 'i' }
+        },
+        {
+            "postal": { $regex: query, $options: 'i' }
+        }
+    ];
+
+    const result = await Building.find({ $or: conditions });
     return result;
 }
 
