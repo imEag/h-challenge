f4b97789a7479af7a5c7f88ca9ae9e89d7fbfb78
Emmanuel <emma145@hotmail.es>
2022-09-04 00:44:37 -0500

    add normalize css. create scss files

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from './components/App';
+import 'normalize.css';
 import './styles/styles.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
