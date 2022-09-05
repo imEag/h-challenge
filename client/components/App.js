150fd76ac3643b3e63596c8705376ec8e293d7ef
imeag <emma145@hotmail.es>
2022-09-05 11:49:17 -0500

    context done

--- client/components/App.js
+++ client/components/App.js
@@ -1,6 +1,5 @@
-import { useState, useContext, useEffect } from "react";
+import { useContext } from "react";
 import { PageContext } from "./context/PageContext";
-import { UserDataContext } from "./context/UserDataContext";
 
 import WelcomePage from './WelcomePage';
 import NamesPage from './NamesPage';
@@ -8,23 +7,23 @@ import AddressPage from './AddressPage';
 import ServicePage from './ServicePage';
 import SubscribePage from './SubscribePage';
 
+const pages = {
+    0: <WelcomePage></WelcomePage>,
+    1: <NamesPage></NamesPage>,
+    2: <AddressPage></AddressPage>,
+    3: <ServicePage></ServicePage>,
+    4: <SubscribePage></SubscribePage>
+}
+
 const App = () => {
 
-    const { page, setPage } = useContext(PageContext);
-    const { userData, setUserData } = useContext(UserDataContext);
-
-    useEffect(() => {
-        console.log('user context: ', userData);
-        console.log('page context: ', page);
-    }, [])
+    const { page } = useContext(PageContext);
 
     return (
         <>
-            <WelcomePage></WelcomePage>
-            <NamesPage></NamesPage>
-            <AddressPage name="Cristian"></AddressPage>
-            <ServicePage></ServicePage>
-            <SubscribePage></SubscribePage>
+            {
+                pages[page] || pages[0]
+            }
         </>
     )
 }
