ea5c000e88bf2d44ba5f16a2665e810b77e7aafc
Emmanuel <emma145@hotmail.es>
2022-09-07 14:08:33 -0500

    add format to phone number in subscribe page component

--- client/components/context/PageContext.js
+++ client/components/context/PageContext.js
@@ -4,7 +4,7 @@ export const PageContext = createContext();
 
 export const PageProvider = ({ children }) => {
 
-    const [page, setPage] = useState(0);
+    const [page, setPage] = useState(4);
 
     return (
         <PageContext.Provider value={{ page, setPage }}>
